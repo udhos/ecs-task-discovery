e73b2490a8e32a135324b3bf1391097a09052ead
udhos <udhos>
2025-02-06 20:23:09 -0300

    Fix tag script.

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,5 +1,5 @@
 
-version=$(go run ./cmd/secrets -version | awk '{ print $2 }' | awk -F= '{ print $2 }')
+version=$(go run ./cmd/ecs-task-discovery-agent -version | awk '{ print $2 }' | awk -F= '{ print $2 }')
 
 git tag v${version}
 git tag chart-${version}
